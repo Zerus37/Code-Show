9f8a318f333fed9c12231d68aa61fb4e38e5def7
Zerus37 <137706134+Zerus37@users.noreply.github.com>
2023-07-27 17:08:04 +0800

    Update PlayerCharacter.cs

--- Multyplayer/PlayerCharacter.cs
+++ Multyplayer/PlayerCharacter.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-
-    public void Move(Vector3 direction)
+	[SerializeField] private float speed = 2f;
+	
+	public void Move(Vector3 direction)
 	{
-        transform.position += direction * Time.deltaTime * speed;
+		transform.position += direction * Time.deltaTime * speed;
 	}
-
+	
 	public void GetMoveInfo(out Vector3 position)
 	{
 		position = transform.position;
